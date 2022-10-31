40926ef7693e223e19544cf2fd4d5b34ccee5b6a
adelguit <adeelguitoun@gmail.com>
2022-10-31 10:15:48 +0100

    Empty the src directory

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const element = <h1>Hello from Create React App</h1>;
+
+ReactDOM.render(element, document.getElementById('root'));
